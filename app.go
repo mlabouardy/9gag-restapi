c4cae5062870ed82d3ff8ee4ce88afb1d12a6283
LABOUARDY Mohamed <mohamed.labouardy@gmail.com>
2017-08-23 11:15:20 +0200

    add coments to the restful api

--- app.go
+++ app.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mlabouardy/9gag"
 )
 
+// Get list of memes by their tag
 func MemesByTagEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	gag9 := gag9.New()
@@ -16,6 +17,7 @@ func MemesByTagEndpoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(memes)
 }
 
+// Expose a new route and start an HTTP server on port 3000
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/memes/{tag}", MemesByTagEndpoint)
